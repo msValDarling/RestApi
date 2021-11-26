0d4063966b558695f34e649f776ee6a09275799b
Mauricio <17030487@itcelaya.edu.mx>
2021-11-25 23:58:04 -0600

    CRUD Tareas

--- rest-api/start/routes.js
+++ rest-api/start/routes.js
@@ -21,10 +21,17 @@ Route.get('/', () => {
 })
 
 Route.group(() => {
+  //Usuarios
   Route.post('usuarios/registro', 'UserController.store');
   Route.post('usuarios/login', 'UserController.login');
+  //Proyectos
   Route.get('proyectos', 'ProyectoController.index').middleware('auth');
   Route.post('proyectos', 'ProyectoController.create').middleware('auth');
   Route.delete('proyectos/:id','ProyectoController.destroy').middleware('auth');
   Route.patch('proyectos/:id','ProyectoController.update').middleware('auth');
+  //Tareas
+  Route.get('proyectos/:id/tareas','TareaController.index').middleware('auth');
+  Route.post('proyectos/:id/tareas','TareaController.create').middleware('auth');
+  Route.patch('tareas/:id','TareaController.update').middleware('auth');
+  Route.delete('tareas/:id','TareaController.destroy').middleware('auth');
 }).prefix('api/v1/');
